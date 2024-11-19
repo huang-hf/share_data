45d6f46e8998f48abad223a88b1d4b4b4906b305
huanghuifeng <huanghuifeng1992@gmail.com>
2024-11-19 17:04:59 +0800

    add sleep time

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 @app.route("/api", methods=["POST"])
 def api(): 
     return jsonify({"message": "Received", "data": request.json})
+
+@app.route("/api-sleep", methods=["POST"])
+def api_sleep():
+    import time
+    time.sleep(3)
+    return jsonify({"message": "Received", "data": request.json})
 app.run(host="0.0.0.0", port=8080)
